aae5b5570fec750b93c9743ba03118ad42729cb7
cronus <bocui552@gmail.com>
2026-07-11 19:47:34 +0800

    delete unused line

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -43,7 +43,6 @@ def test_max(t: Tensor) -> None:
     #print(max(t.contiguous().view(t.size).to_numpy()))
     assert_close(max_value, max(t.contiguous().view(t.size).to_numpy()))
 
-    #minitorch.grad_check(lambda a: minitorch.max(a), t)
 
 @pytest.mark.task4_4
 @given(tensors(shape=(1, 1, 4, 4)))
